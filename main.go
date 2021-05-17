fa568b00e7c9827892af90b1332e5859981f7897
Jack Dockerty <jdockerty19@gmail.com>
2021-05-17 20:19:47 +0100

    Add getAmazonMessages func

--- main.go
+++ main.go
@@ -115,6 +115,31 @@ func amazonEmailIds(c *client.Client) []uint32 {
 // 	}
 // }
 
+func getAmazonMessages(c *client.Client, ids []uint32, section imap.BodySectionName) <-chan *imap.Message {
+	// Create a set of UIDs for the emails, each email has a specific ID associated with it
+	seqSet := new(imap.SeqSet)
+
+	// Add the ids of the Amazon messages which can be parsed for Kindle note emails later
+	seqSet.AddNum(ids...)
+
+	// Get the whole message body
+	items := []imap.FetchItem{section.FetchItem()}
+
+	// Bufferred channel for the last 10 messages
+	// NOTE: Could make this user configurable in the future?
+	messages := make(chan *imap.Message, 10)
+
+	// Run separate goroutine for fetching messages, these are
+	// passed back over the channel defined above
+	go func() {
+		if err := c.Fetch(seqSet, items, messages); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return messages
+}
+
 func main() {
 
 	// var wg sync.WaitGroup
@@ -146,27 +171,8 @@ func main() {
 
 	ids := amazonEmailIds(c)
 
-	// Create a set of UIDs for the emails, each email has a specific ID associated with it
-	seqSet := new(imap.SeqSet)
-
-	// Add the ids of the Amazon messages which can be parsed for Kindle note emails later
-	seqSet.AddNum(ids...)
-
-	// Get the whole message body
 	var section imap.BodySectionName
-	items := []imap.FetchItem{section.FetchItem()}
-
-	// Bufferred channel for the last 10 messages
-	// NOTE: Could make this user configurable in the future?
-	messages := make(chan *imap.Message, 10)
-
-	// Run separate goroutine for fetching messages, these are
-	// passed back over the channel defined above
-	go func() {
-		if err := c.Fetch(seqSet, items, messages); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	messages := getAmazonMessages(c, ids, section)
 
 	// Loop over the messages from the channel
 	for m := range messages {
