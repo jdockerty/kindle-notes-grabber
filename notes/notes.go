f5a07306a3c6c3a7385d8dbc0e916f4b628562f4
Jack Dockerty <jdockerty19@gmail.com>
2021-05-30 11:46:38 +0100

    Update function for interface usage from testing research

--- notes/notes.go
+++ notes/notes.go
@@ -1,7 +1,15 @@
 package notes
 
+import (
+	"log"
 
+	"github.com/emersion/go-imap"
+	_ "github.com/emersion/go-imap/client"
+)
 
+type imapClient interface {
+	Search(criteria *imap.SearchCriteria) ([]uint32, error)
+}
 
 type Note struct {
 	Type       string
@@ -10,14 +18,35 @@ type Note struct {
 	Starred    bool
 }
 
-
 type Notes struct {
 	Author string
 	Title  string
 	Notes  []Note
 }
 
+func (n *Notes) GetEmailIds(c imapClient, sc *imap.SearchCriteria) []uint32 {
+	// criteria := imap.NewSearchCriteria()
+
+	// TODO: Implement a customisable time range for when to check for.
+	// twoDaysAgo := time.Now().AddDate(0, 0, -2)
+	// criteria.SentSince = twoDaysAgo
+
+	// TODO: Look into searching via IMAP, this doesn't seem to work
+	// as expected when looking for value in the email subject, will parse
+	// subject manually for now.
+	// subjSearch := "OR SUBJECT \"Your Kindle Notes\""
+
+	// fromAmazon := "FROM no-reply@amazon.com"
+	// criteria.Body = []string{fromAmazon}
+
+	ids, err := c.Search(sc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Got Ids:", ids)
+	return ids
+}
+
 func New() *Notes {
 	return &Notes{}
 }
-
