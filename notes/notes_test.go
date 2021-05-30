98dc21957b21f2889ce7a982124f70837625f1a7
Jack Dockerty <jdockerty19@gmail.com>
2021-05-30 15:18:28 +0100

    Add mock function for Fetch for Amazon emails

--- notes/notes_test.go
+++ notes/notes_test.go
@@ -8,12 +8,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var SearchMock func() ([]uint32, error)
-
 type mockClient struct{}
 
+// Search is the mocked IMAP 'Search' function, this ensures that the mockClient satisfies
+// the imapClient interface by implementing the correct method. 
 func (mc mockClient) Search(search *imap.SearchCriteria) ([]uint32, error) {
-	return SearchMock()
+	return []uint32{1, 2, 3}, nil
+}
+
+// Fetch is the mocked IMAP 'Fetch' function, this ensures that the mockClient satisfies
+// the imapClient interface by implementing the correct method. 
+func (mc mockClient) Fetch(seqset *imap.SeqSet, items []imap.FetchItem, ch chan *imap.Message) error {
+	fakeMsg := &imap.Message{}
+	ch <- fakeMsg
+	return nil
 }
 
 func TestGetEmailIds(t *testing.T) {
@@ -21,16 +29,24 @@ func TestGetEmailIds(t *testing.T) {
 
 	n := notes.New()
 
-	SearchMock = func() ([]uint32, error) {
-		return []uint32{1, 2, 3}, nil
-	}
-
-	// Can pass nil as search criteria as this takes a pointer to imap.SearchCriteria
-	// which nil satisfies.
+	// Can pass nil as search criteria as this takes a pointer to imap.SearchCriteria, which nil satisfies.
 	ids := n.GetEmailIds(m, nil)
 
 	var uint32Slice []uint32
 	assert.IsType(t, uint32Slice, ids)
+}
+
+func TestGetAmazonMessages(t *testing.T) {
+	assert := assert.New(t)
+	var m mockClient
+
+	var section imap.BodySectionName
+	n := notes.New()
+	fakeIds := []uint32{1, 2, 3}
+	msgs := n.GetAmazonMessages(m, fakeIds, section)
+
+	var receiveChannelType <-chan *imap.Message
+	assert.IsType(receiveChannelType, msgs)
 }
 
 func TestGetNewNotesDefaults(t *testing.T) {
