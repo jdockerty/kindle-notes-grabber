7483c4e2566b89e397434ae5eb4c6d117b2e255a
Jack Dockerty <jdockerty19@gmail.com>
2021-05-30 11:46:19 +0100

    Add GetEmailIds test using mock

--- notes/notes_test.go
+++ notes/notes_test.go
@@ -3,30 +3,31 @@ package notes_test
 import (
 	"testing"
 
+	"github.com/emersion/go-imap"
 	"github.com/jdockerty/kindle-notes-grabber/notes"
-
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	// "github.com/stretchr/testify/suite"
 )
 
-type mockNotes struct {
-	N notes.Notes
-	mock.Mock
-}
+var SearchMock func() ([]uint32, error)
 
-type MockNotes interface {
-	GetEmailIds() []uint32
-}
+type mockClient struct{}
 
-// WIP: Implement mocked function response for Ids?
+func (mc mockClient) Search(search *imap.SearchCriteria) ([]uint32, error) {
+	return SearchMock()
+}
 
 func TestGetEmailIds(t *testing.T) {
+	var m mockClient
 
-	// notes := notes.New()
-	// ids := mockNotes.GetEmailIds()
-	// ids := mockNotes.GetEmailIds()
-	ids := MockNotes.GetEmailIds()
+	n := notes.New()
+
+	SearchMock = func() ([]uint32, error) {
+		return []uint32{1, 2, 3}, nil
+	}
+
+	// Can pass nil as search criteria as this takes a pointer to imap.SearchCriteria
+	// which nil satisfies.
+	ids := n.GetEmailIds(m, nil)
 
 	var uint32Slice []uint32
 	assert.IsType(t, uint32Slice, ids)
@@ -43,5 +44,4 @@ func TestGetNewNotesDefaults(t *testing.T) {
 	assert.Empty(testNotes.Title, blankString)
 	assert.Empty(testNotes.Notes, blankNotes)
 
-	
 }
