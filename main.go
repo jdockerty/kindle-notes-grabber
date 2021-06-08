53613150b4b96613df09d89d6e0d8a769e71ef10
Jack Dockerty <jdockerty19@gmail.com>
2021-06-08 19:43:09 +0100

    Remove commented functions from main

--- main.go
+++ main.go
@@ -18,19 +18,6 @@ import (
 	"github.com/jdockerty/kindle-notes-grabber/notes"
 )
 
-// type Note struct {
-// 	Type       string
-// 	Location   string
-// 	Annotation string
-// 	Starred    bool
-// }
-
-// type Notes struct {
-// 	Author string
-// 	Title  string
-// 	Notes  []Note
-// }
-
 const (
 
 	// Index positions of the relevant records, these are the column headings in the CSV file.
@@ -39,76 +26,6 @@ const (
 	starredIndex    int = 2
 	annotationIndex int = 3
 )
-
-// func amazonEmailIds(c *client.Client) []uint32 {
-
-// 	criteria := imap.NewSearchCriteria()
-
-// 	// TODO: Implement a customisable time range for when to check for.
-// 	// twoDaysAgo := time.Now().AddDate(0, 0, -2)
-// 	// criteria.SentSince = twoDaysAgo
-
-// 	// TODO: Look into searching via IMAP, this doesn't seem to work
-// 	// as expected when looking for value in the email subject, will parse
-// 	// subject manually for now.
-// 	// subjSearch := "OR SUBJECT \"Your Kindle Notes\""
-
-// 	fromAmazon := "FROM no-reply@amazon.com"
-// 	criteria.Body = []string{fromAmazon}
-
-// 	ids, err := c.Search(criteria)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	log.Println("Got Ids:", ids)
-// 	return ids
-
-// }
-
-// func getAmazonMessages(c *client.Client, ids []uint32, section imap.BodySectionName) <-chan *imap.Message {
-// 	// Create a set of UIDs for the emails, each email has a specific ID associated with it
-// 	seqSet := new(imap.SeqSet)
-
-// 	// Add the ids of the Amazon messages which can be parsed for Kindle note emails later
-// 	seqSet.AddNum(ids...)
-
-// 	// Get the whole message body
-// 	items := []imap.FetchItem{section.FetchItem()}
-
-// 	// Bufferred channel for the last 10 messages
-// 	// NOTE: Could make this user configurable in the future?
-// 	messages := make(chan *imap.Message, 10)
-
-// 	// Run separate goroutine for fetching messages, these are
-// 	// passed back over the channel defined above
-// 	go func() {
-// 		if err := c.Fetch(seqSet, items, messages); err != nil {
-// 			log.Fatal(err)
-// 		}
-// 	}()
-
-// 	return messages
-// }
-
-// func getMailReaders(messages <-chan *imap.Message, section imap.BodySectionName) []*mail.Reader {
-
-// 	var mailReaders []*mail.Reader
-// 	// Loop over the messages from the channel
-// 	for m := range messages {
-
-// 		messageBody := m.GetBody(&section)
-
-// 		mailReader, err := mail.CreateReader(messageBody)
-// 		if err != nil {
-// 			log.Println("Using unknown charset for reading mail header.")
-// 		}
-
-// 		mailReaders = append(mailReaders, mailReader)
-
-// 	}
-
-// 	return mailReaders
-// }
 
 // parseNotes is used to create a temporary CSV file which can be
 // read from. This is done as it provides a simpler mechanicm than directly
