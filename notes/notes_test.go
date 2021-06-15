2875d130a60831f467a597a665a4593261bf4d02
Jack <56563911+jdockerty@users.noreply.github.com>
2021-06-15 21:16:57 +0100

    Update/struct usage (#3)
    * Add initial defaults test
    
    * Add GetEmailIds test using mock
    
    * Update function for interface usage from testing research
    
    * Update main calls
    
    * Add mock function for Fetch for Amazon emails
    
    * Update implementation to use Notes struct as receiver
    
    * Refactor main implementation of struct Fetch changes
    
    * Run go fmt
    
    * Add relevant docstrings to functions and structs
    
    * Update use of Empty assertion to correct usage
    
    * Remove To-Do's from README, contained within GitHub project
    
    * Add GetMailReaders function
    
    * Update main usage for new MailReader func
    
    * Run go fmt
    
    * Update main usage for removal of locally scoped structs
    
    * Remove commented functions from main
    
    * Remove author from Notes struct
    
    * Update test for author
    
    * Add Populate function to mutate notes struct
    
    * Update imports
    
    * Run go fmt
    
    * Update variable naming for clarity
    
    * Update docstring for mockClient
    
    * Add Write to file test
    
    * Add initial implementation of write for test passing
    
    * Update variables into constants where necessary
    
    * Add docstring
    
    * Update README

---
+++ notes/notes_test.go
@@ -0,0 +1,91 @@
+package notes_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/emersion/go-imap"
+	"github.com/jdockerty/kindle-notes-grabber/notes"
+	"github.com/stretchr/testify/assert"
+)
+
+// mockClient is an empty struct used as a fake IMAP client for
+// satisfying the respective interface of the 'notes' package.
+type mockClient struct{}
+
+// Search is the mocked IMAP 'Search' function, this ensures that the mockClient satisfies
+// the imapClient interface by implementing the correct method.
+func (mc mockClient) Search(search *imap.SearchCriteria) ([]uint32, error) {
+	return []uint32{1, 2, 3}, nil
+}
+
+// Fetch is the mocked IMAP 'Fetch' function, this ensures that the mockClient satisfies
+// the imapClient interface by implementing the correct method.
+func (mc mockClient) Fetch(seqset *imap.SeqSet, items []imap.FetchItem, ch chan *imap.Message) error {
+	fakeMsg := &imap.Message{}
+	ch <- fakeMsg
+	return nil
+}
+
+func getFakeNotesData() *notes.Notes {
+	dummyNotes := notes.New()
+	
+	var testNote notes.Note
+	testNote.Annotation = "Annotation used in test"
+	testNote.Location = "Page 1"
+	testNote.Starred = false
+	testNote.Type = "Highlight"
+
+	dummyNotes.Notes = append(dummyNotes.Notes, testNote)
+	
+	return dummyNotes
+}
+func TestGetEmailIds(t *testing.T) {
+	var m mockClient
+
+	n := notes.New()
+
+	// Can pass nil as search criteria as this takes a pointer to imap.SearchCriteria, which nil satisfies.
+	ids := n.GetEmailIds(m, nil)
+
+	var uint32Slice []uint32
+	assert.IsType(t, uint32Slice, ids)
+}
+
+func TestGetAmazonMessages(t *testing.T) {
+	assert := assert.New(t)
+	var m mockClient
+
+	var section imap.BodySectionName
+	n := notes.New()
+	fakeIds := []uint32{1, 2, 3}
+	msgs := n.GetAmazonMessages(m, fakeIds, section)
+
+	var receiveChannelType <-chan *imap.Message
+	assert.IsType(receiveChannelType, msgs)
+}
+
+func TestGetNewNotesDefaults(t *testing.T) {
+	assert := assert.New(t)
+	testNotes := notes.New()
+
+	assert.Empty(testNotes.Title, "A newly created Notes struct should contain a blank string for the 'Title'")
+	assert.Empty(testNotes.Notes, "A newly created Notes struct should not contain any populated Note structs within the slice")
+
+}
+
+func TestWriteNoteFile(t *testing.T) {
+	assert := assert.New(t)
+
+	testNotes := getFakeNotesData()
+
+	fileName := fmt.Sprintf("%s.yaml", testNotes.Title)
+	
+	defer os.Remove(fileName)
+	
+	i, err := notes.Write(testNotes)
+	assert.Nil(err)
+	assert.Greater(i, 0, "The number of bytes written should be greater than 0")
+
+}
