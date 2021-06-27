a0d0beeef21217ce7033c8c60dc0c762500292a4
Jack Dockerty <jdockerty19@gmail.com>
2021-06-27 19:32:51 +0100

    Redesign config package to use viper

--- config/config.go
+++ config/config.go
@@ -1,34 +1,62 @@
 package config
 
 import (
-	"errors"
-	"fmt"
-	"io/fs"
 	"log"
+	"os"
 
-	"github.com/ilyakaznacheev/cleanenv"
+	// "github.com/ilyakaznacheev/cleanenv"
+	"github.com/spf13/viper"
 )
 
 type Config struct {
-	Email    string `yaml:"email" env:"KNG_EMAIL"`
-	Password string `yaml:"password" env:"KNG_PASSWORD"`
+	Email    string `mapstructure:"email" env:"KNG_EMAIL"`
+	Password string `mapstructure:"password" env:"KNG_PASSWORD"`
 }
 
 // New uses the 'cleanenv' package to read from the relevant configuration file,
 // acting as a wrapper for the error and returning a correctly configured struct.
 func New(path string) (*Config, error) {
 
-	// Return an error when the provided path is not valid.
-	if isValidPath := fs.ValidPath(path); !isValidPath {
-		errorMsg := fmt.Sprintf("Invalid path provided: %s", path)
-		return nil, errors.New(errorMsg)
-	}
+	// // Return an error when the provided path is not valid.
+	// if isValidPath := fs.ValidPath(path); !isValidPath {
+	// 	errorMsg := fmt.Sprintf("Invalid path provided: %s", path)
+	// 	return nil, errors.New(errorMsg)
+	// }
 
 	var cfg Config
-	err := cleanenv.ReadConfig(path, &cfg)
+	log.Println("Passed path", path)
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatalf("Configuration is not set: %s", err)
+		return nil, err
 	}
 
+	viper.SetEnvPrefix("KNG")
+	viper.AutomaticEnv()
+
+	viper.AddConfigPath(".")
+	viper.AddConfigPath(path)
+	viper.AddConfigPath(homeDir)
+
+	viper.SetConfigName("kng-config")
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; Attempt to read from environemnt
+			viper.Set("EMAIL", viper.Get("KNG_EMAIL"))
+			viper.Set("PASSWORD", viper.Get("KNG_PASSWORD"))
+
+		} else {
+			// Config file was found but another error was produced
+			return nil, err
+		}
+	}
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("USING CONFIG", cfg)
 	return &cfg, nil
 }
