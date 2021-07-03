73eb0f602ac27748fa33fdcfc490ede51682e9f3
Jack Dockerty <jdockerty19@gmail.com>
2021-07-03 22:54:34 +0100

    Update main usage for cobra integration

--- main.go
+++ main.go
@@ -1,88 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/emersion/go-imap"
-	"github.com/emersion/go-imap/client"
-	"github.com/jdockerty/kindle-notes-grabber/config"
-	"github.com/jdockerty/kindle-notes-grabber/notes"
-)
-
-const (
-	configPath = "kng-config.yaml"
-	fromAmazon = "FROM no-reply@amazon.com"
-	mailbox    = "INBOX"
+	"github.com/jdockerty/kindle-notes-grabber/cmd"
 )
 
 func main() {
-
-	conf, err := config.New(configPath)
-	if err != nil {
-		log.Fatalf("Cannot read configuration: %s", err)
-	}
-	log.Println("Connecting to server...")
-
-	// Connect to server
-
-	var im config.IMAPServer
-	im.Populate("gmail")
-	c, err := client.DialTLS(im.Socket, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Connected")
-
-	// Don't forget to logout
-	defer c.Logout()
-
-	// Login
-	if err := c.Login(conf.Email, conf.Password); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Logged in")
-
-	_, err = c.Select(mailbox, false)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// NOTE: Hard-coded criteria for now.
-	criteria := *imap.NewSearchCriteria()
-	criteria.Body = []string{fromAmazon}
-
-	ids := notes.GetEmailIds(c, &criteria)
-
-	var notesCollection []*notes.Notes
-	var section imap.BodySectionName
-
-	completedBooks, err := notes.LoadCompletedBooks()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, id := range ids {
-		myNotes := notes.New()
-		messages := myNotes.GetAmazonMessage(c, id, section)
-		mailReaders := myNotes.GetMailReaders(messages, section)
-		myNotes.Populate(mailReaders)
-
-		// If the title exists in the map, skip it
-		if _, ok := (*completedBooks)[myNotes.Title]; ok {
-			log.Printf("%s already seen", myNotes.Title)
-			continue
-		}
-
-		log.Printf("Adding %s to map", myNotes.Title)
-		(*completedBooks)[myNotes.Title] = struct{}{}
-
-		notes.Write(myNotes)
-		notesCollection = append(notesCollection, myNotes)
-	}
-
-	err = notes.Save(notesCollection)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Done!")
+	cmd.Execute()
 }
